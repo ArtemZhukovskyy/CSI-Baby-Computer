f2bfe6baac72ce7ff917c67684d8b2bf922901b5
201220830 <201220830@lbschools.net>
2026-09-21 18:02:00 -0700

    Resolve merge conflict w/ phase integration program additions

---
+++ Phase-Integration-Programs/eepromTest.asm
@@ -0,0 +1,6 @@
+;   refer to src: Ben Eater - asy lang v. machine code pt. 3 @ 12:44 https://www.youtube.com/watch?v=oO8_2JJV0B4
+
+;   to do:
+;   - add program origin memory location
+;   - set accumulator A to act as LED blinker via jmp
+;   - pad file to fit ROM size
---
+++ Phase-Integration-Programs/lcdTest.asm
@@ -0,0 +1,3 @@
+;   to do:
+;   - v1: print out a singular character on LCD screen
+;   - v2: print out entire string of characters on screen
