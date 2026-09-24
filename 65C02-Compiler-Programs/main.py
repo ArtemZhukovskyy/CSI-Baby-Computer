f4d271b4e84c4d2ebaeb649dd9a10ba0e4e05622
201220830 <201220830@lbschools.net>
2026-09-23 21:33:12 -0700

    add compiler codespace

---
+++ 65C02-Compiler-Programs/main.py
@@ -0,0 +1,8 @@
+"""
+TODO:
+- Prompt student to import python file for conversion
+- Parse file input for keywords
+- For each keyword, call associated parse and replace file
+  - Most likely written as a class
+  - For more details, refer to project proposal (as of now not public)
+"""
